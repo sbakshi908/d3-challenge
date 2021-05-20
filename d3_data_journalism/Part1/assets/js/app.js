4577beff09ac9f2bf40cf4737de75bc492f0bac1
Summya Bakshi <summyabakshi@gmail.com>
2021-05-20 14:04:05 -0400

    static graph complete

--- d3_data_journalism/Part1/assets/js/app.js
+++ d3_data_journalism/Part1/assets/js/app.js
@@ -1,4 +1,5 @@
 // @TODO: YOUR CODE HERE!
+
 var svgWidth = 960;
 var svgHeight = 500;
 
@@ -13,16 +14,16 @@ var width = svgWidth - margin.left - margin.right;
 var height = svgHeight - margin.top - margin.bottom;
 
 // Create an SVG wrapper, append an SVG group that will hold our chart, and shift the latter by left and top margins.
-var svg = d3.select(".scatter")
+var svg = d3.select("#scatter")
   .append("svg")
   .attr("width", svgWidth)
   .attr("height", svgHeight);
 
-var chartGroup = svg.append("g")
+var chartGroup = svg.append("g") //apends as a group 
   .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
 // Import Data
-d3.csv("data.csv").then(function (smokerData) {
+d3.csv("assets/Data/data.csv").then(function (smokerData) {
     //parse data as numbers 
     smokerData.forEach(function (data) {
       data.poverty=+ data.poverty;
@@ -32,8 +33,9 @@ d3.csv("data.csv").then(function (smokerData) {
     //create the scale functions 
 
     var xLinearScale = d3.scaleLinear()
-      .domain([20, d3.max(smokerData, d => d.poverty])
-      .range([0, width]);
+      //.domain([20, d3.max(smokerData, d => d.poverty)])
+      .domain([d3.min(smokerData, d=> d.poverty)* 0.9 , d3.max(smokerData, d => d.poverty) *1.1])
+      .range([0, width]); //scale data across pixels 
 
     var yLinearScale = d3.scaleLinear()
       .domain([0, d3.max(smokerData, d => d.healthcare)])
@@ -43,6 +45,13 @@ d3.csv("data.csv").then(function (smokerData) {
     var bottomAxis = d3.axisBottom(xLinearScale);
     var leftAxis = d3.axisLeft(yLinearScale);
 
+    //create axis 
+    chartGroup.append("g")
+      .attr("transform", `translate(0, ${height})`)
+      .call(bottomAxis);
+    chartGroup.append("g")
+      .call(leftAxis);
+
     //cerate the circles for plotting 
     
     var circlesGroup = chartGroup.selectAll("circle")
@@ -51,18 +60,29 @@ d3.csv("data.csv").then(function (smokerData) {
     .append("circle")
     .attr("cx", d => xLinearScale(d.poverty))
     .attr("cy", d => yLinearScale(d.healthcare))
-    .attr("r", "15")
+    .attr("r", "17")
     .attr("fill", "blue")
     .attr("opacity", ".5");
 
+    //create text group 
+    var textGroup = chartGroup.selectAll(null)
+    .data(smokerData)
+    .enter()
+    .append("text")
+    .attr("x", d => xLinearScale(d.poverty))
+    .attr("y", d => yLinearScale(d.healthcare))
+    .text(d => d.abbr)
+    .style("text-anchor", "middle");
+
     //Initialize tool tip
   
-    // var toolTip = d3.tip()
-    // .attr("class", "tooltip")
-    // .offset([80, -60])
-    // .html(function(d) {
-    //   return (`${d.rockband}<br>Hair length: ${d.hair_length}<br>Hits: ${d.num_hits}`);
-    // });
+    var toolTip = d3.tip()
+    .attr("class", "tooltip")
+    .offset([80, -60])
+    .html(function(d) {
+    return (`${d.state}<br>Poverty %: ${d.poverty}<br>Obesity%: ${d.obesity}`);
+    });
+    chartGroup.call(toolTip);
   
 });
 
